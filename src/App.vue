8d02c0212fff1ab75d322b8edd37fd1037788533
gorik0 <delavarus@gmail.com>
2024-07-09 10:13:47 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Gorik from './Components/Gorik.vue'
+</script>
+
+<template>
+  <h1>Helo gorsik</h1>
+  <Gorik />
+</template>
---
+++ src/Components/Gorik.vue
@@ -0,0 +1,9 @@
+<template>
+  <p class="body">Helo from component gorik!!!</p>
+</template>
+
+<style scoped>
+.body {
+  background: red;
+}
+</style>
